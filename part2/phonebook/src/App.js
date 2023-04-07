d6ec71be36155bf9adf5218c09f5dd962bbc3b33
PeterArbaugh <PeterArbaugh@users.noreply.github.com>
2023-04-06 22:00:32 -0700

    exercise 2.13

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 
 const Search = (props) => (
     <div>
@@ -52,13 +52,12 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons =>{
+        setPersons(initialPersons)
       })
-  }, [])
+    })
   console.log('render', persons.length, 'notes')
 
   const handleNameChange = (event) => {
@@ -88,10 +87,10 @@ const App = () => {
         number: newNumber,
         //id: persons.length + 1
       }
-      axios
-        .post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
           console.log(persons)
